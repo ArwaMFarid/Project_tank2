aed1419cd605762b393ac7776cb31e3530fd4093
ArwaMFarid <121234918+arwamfarid@users.noreply.github.com>
2022-12-23 17:44:37 +0200

    Initial commit

--- src/tank/Start.java
+++ src/tank/Start.java
@@ -4,10 +4,51 @@
  */
 package tank;
 
+import java.awt.event.KeyEvent;
+import javax.media.opengl.GLAutoDrawable;
+import tank.Texture.AnimListener;
+import tank.Texture.TextureReader;
 /**
  *
  * @author MSI-
  */
-public class Start {
+public class Start extends AnimListener {
     
+    String textureNames[] = {"Man1.png", "Man2.png", "Man3.png", "Man4.png", "Back.png"};
+    TextureReader.Texture texture[] = new TextureReader.Texture[textureNames.length];
+    int textures[] = new int[textureNames.length];
+    @Override
+    public void init(GLAutoDrawable gld) {
+
+    }
+
+    @Override
+    public void display(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
+
+    }
+
+    @Override
+    public void displayChanged(GLAutoDrawable glAutoDrawable, boolean b, boolean b1) {
+
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
 }
--- src/tank/Tank.java
+++ src/tank/Tank.java
@@ -1,10 +1,34 @@
 package tank;
+import com.sun.opengl.util.*;
+import java.awt.*;
+import javax.media.opengl.*;
+import javax.swing.*;
+import tank.Texture.AnimListener;
 
-
-public class Tank {
+public class Tank extends JFrame {
 
     public static void main(String[] args) {
-      
+        new Tank(new Start());
     }
-    
+    public Tank(AnimListener aListener) {
+        GLCanvas glcanvas;
+        Animator animator;
+
+        AnimListener listener = aListener;
+        glcanvas = new GLCanvas();
+        glcanvas.addGLEventListener(listener);
+        glcanvas.addKeyListener(listener);
+        getContentPane().add(glcanvas, BorderLayout.CENTER);
+        animator = new FPSAnimator(15);
+        animator.add(glcanvas);
+        animator.start();
+
+        setTitle("Anim Test");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(900, 500);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setFocusable(true);
+        glcanvas.requestFocus();
+    }
 }
